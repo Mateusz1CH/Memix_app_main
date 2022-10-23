4622ffa7275f651d5cd3f3c1b1cfb3c9efb0336d
Mateo <mateusz1cholewa@gmail.com>
2022-10-23 20:27:09 +0200

    add memes from api, refresh app every 30sec

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def get_meme():
 @app.route("/")
 def index():
     meme, subreddit = get_meme()
-    return render_template("index.html")
+    return render_template("index.html", meme=meme, subreddit=subreddit)
 
 app.run(host="0.0.0.0", port=80)
