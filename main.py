fc11a03b56f0c7ab9682183a2dff162406de6a82
Mateo <mateusz1cholewa@gmail.com>
2022-10-23 19:49:59 +0200

    define function to get memes

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
+def get_meme():
+    url = "https://meme-api.herokuapp.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme = response["preview"][-2]
+    subreddit = response['subreddit']
+    return meme, subreddit
