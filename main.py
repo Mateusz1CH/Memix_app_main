6ef890dc9b3d109500722f249307565dd62a8757
Mateusz <42621951+Mateusz1CH@users.noreply.github.com>
2023-11-16 08:12:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ import json
 
 app = Flask(__name__)
 
-def get_meme():
+def get_meme_old():
     url = "https://meme-api.com"
     response = json.loads(requests.request("GET", url).text)
     meme = response["preview"][-2]
     subreddit = response['subreddit']
     return meme, subreddit
+def get_meme():
+    url = "https://meme-api.com"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+        data = response.json()
+        meme_url = data.get("url", "Default URL")  # Get the direct URL of the meme
+        subreddit = data.get('subreddit', 'Unknown Subreddit')  # Get the subreddit
+    except requests.RequestException as e:
+        print(f"Error fetching meme: {e}")
+        meme_url, subreddit = "Error", "Error"
+    return meme_url, subreddit
 
 @app.route("/")
 def index():
